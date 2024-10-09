4fa1bdbca25ac9c353fae6000e3b31d38a40ead4
Filippospapageorgiou <filpap9@gmail.com>
2024-10-09 19:56:42 +0300

    test push branch filipe

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1 class="mx-auto w-max text-3xl font-bold underline">
-  Svelte Works and <span class="text-3xl text-blue-600">Tailwind</span>
+	Svelte Works and <span class="text-3xl text-blue-600">Tailwind</span>
 </h1>
-
+<h1 class="text-4xl text-red-500">YOLO</h1>
